b0902b06d2231a1381dc2a7637b3bcac460e84ad
AlysonRyan01 <alysonullirsch8@gmail.com>
2025-10-31 12:17:03 -0300

    feat: inicializando o frontend com angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import { Routes } from '@angular/router';
-import { CustomersComponent } from './pages/customer.page/customer.page';
-import { StockComponent } from './pages/stock.page/stock.page';
-import { InvoicePage } from './pages/invoice.page/invoice.page';
 
-export const routes: Routes = [
-  { path: 'clientes', component: CustomersComponent },
-  { path: 'estoque', component: StockComponent },
-  { path: 'notas-fiscais', component: InvoicePage },
-  { path: '', redirectTo: 'clientes', pathMatch: 'full' },
-];
+export const routes: Routes = [];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideToastr } from 'ngx-toastr';
-
-provideToastr({
-  timeOut: 3000,
-  positionClass: 'toast-top-right',
-  preventDuplicates: true,
-  progressBar: true,
-  tapToDismiss: false,
-  closeButton: true
-});
-
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
